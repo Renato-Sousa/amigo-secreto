e450fab8d96b57c30fb63093976bd99d217330f4
Renato-Sousa <renatosoureis@gmail.com>
2025-02-17 19:09:22 -0500

    teste

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver
+function adicionarAmigo() {
+    alert (document.getElementById('amigo').innerHTML)
+    document.getElementById('amigo').innerText
+
+}
