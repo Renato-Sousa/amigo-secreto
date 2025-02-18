f7ca94c693a00872825654bcb4dcdb2e3134c84d
Renato-Sousa <renatosoureis@gmail.com>
2025-02-17 22:39:35 -0500

    1er versao

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-function adicionarAmigo() {
-    alert (document.getElementById('amigo').innerHTML)
-    document.getElementById('amigo').innerText
+let inputAmigo = document.getElementById ('amigo');
 
-}
+let listaAmigos = [];
+
+let ulListaAmigos = document.getElementById ('listaAmigos');
+let ulResultado = document.getElementById ('resultado');
+
+function adicionarAmigo(){
+    if (inputAmigo.value == ""){
+        alert('Por favor, insira um nome.');
+    }
+    else {
+        listaAmigos.push(inputAmigo.value);
+   // ulListaAmigos.innerHTML = ulListaAmigos.innerHTML + inputAmigo.value
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+    }
+
+ }
+    
+ function sortearAmigo() {
+    let random = Math.floor(Math.random() * listaAmigos.length);
+     let amigoSecreto = listaAmigos[random];
+     ulResultado.innerHTML = `<li>Seu amigo secreto e : ${amigoSecreto}</li>`;
+    
+     }
+
